1f504bc06f8233186de3c0d7e634b541278486bf
dellumiquinga4 <dellumiquinga4@espe.edu.ec>
2024-12-14 08:48:03 -0500

    1

---
+++ ExamenLlumiquinga/src/main/java/org/example/examen/examenllumiquinga/ExamenLlumiquingaApplication.java
@@ -0,0 +1,13 @@
+package org.example.examen.examenllumiquinga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenLlumiquingaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExamenLlumiquingaApplication.class, args);
+    }
+
+}
---
+++ ExamenLlumiquinga/src/test/java/org/example/examen/examenllumiquinga/ExamenLlumiquingaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.examen.examenllumiquinga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenLlumiquingaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
